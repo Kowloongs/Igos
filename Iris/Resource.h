e257dd84e470fb90235f24772f1393447296b73f
Carlyon W. Tung <loongs@syuusui.com>
2018-11-24 22:43:37 +0800

    Bitmap

---
+++ Iris/Resource.h
@@ -0,0 +1,27 @@
+// {{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Iris.rc
+//
+#define IDB_BACKBORAD                   121
+#define IDB_BOARD                       122
+#define IDB_STONE                       123
+#define IDB_TOOLBAR                     124
+#define IDB_DISABLE                     125
+#define IDB_HOVER                       126
+#define IDB_NORMAL                      127
+#define IDR_WAVE_INVALID                128
+#define IDR_WAVE_LOSE                   129
+#define IDR_WAVE_MOVE                   130
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+	#ifndef APSTUDIO_READONLY_SYMBOLS
+		#define _APS_NEXT_RESOURCE_VALUE        111
+		#define _APS_NEXT_COMMAND_VALUE         40001
+		#define _APS_NEXT_CONTROL_VALUE         1000
+		#define _APS_NEXT_SYMED_VALUE           101
+	#endif
+#endif
+
+
